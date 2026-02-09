d7a17effb0f27b1f08b2d824948782ddbf4455a1
aritra-chy <seoaritra2002@gmail.com>
2026-02-09 09:10:01 +0600

    committed

---
+++ 69.sqrt-x.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=69 lang=cpp
+ *
+ * [69] Sqrt(x)
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int mySqrt(int x) {
+        long r=x;
+        while(r*r>x)
+        {
+            r=(r+x/r)/2;
+        }
+        return r;
+    }
+};
+// @lc code=end
+
