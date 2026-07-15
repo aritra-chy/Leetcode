022a60ced2745cc301ec57376d15a4196452ecbb
aritra-chy <seoaritra2002@gmail.com>
2026-07-15 12:44:06 +0600

    solved

---
+++ 70.climbing-stairs.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=70 lang=cpp
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 2) return n;
+        int a = 1, b=2;
+        for(int i=3; i<=n; i++)
+        {
+            int c = a+b;
+            a=b;
+            b=c;
+        }
+        return b;
+    }
+};
+// @lc code=end
+
